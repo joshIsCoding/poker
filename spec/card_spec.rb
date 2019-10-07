085a9880a097411d17ff2ddf2a177e2601852c4b
joshIsCoding <yes_you_do@hotmail.com>
2019-10-07 20:42:19 +0100

    Add Card#initialize specs

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -2,6 +2,13 @@ require 'card'
 require 'rspec'
 
 describe "Card#initialize" do
+   subject(:ace_spades) { Card.new(:spades, :A) }
+
+   it "initializes the card with the correct suit and card type" do
+      expect(ace_spades.suit).to eq(:spades)
+      expect(ace_spades.type).to eq(:A)
+
+   end
 
 
 
