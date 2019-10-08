bd8e33af87dfb3a14a476da31421a9ee06b5bbf6
joshIsCoding <yes_you_do@hotmail.com>
2019-10-08 23:42:23 +0100

    Wrap tests within Card class description and add Card#to_s spec

--- spec/card_spec.rb
+++ spec/card_spec.rb
@@ -1,15 +1,24 @@
 require 'card'
 require 'rspec'
 
-describe "Card#initialize" do
-   subject(:ace_spades) { Card.new(:spades, :A) }
+describe Card do
+   describe "Card#initialize" do
+      subject(:ace_spades) { Card.new(:spades, :A) }
 
-   it "initializes the card with the correct suit and card type" do
-      expect(ace_spades.suit).to eq(:spades)
-      expect(ace_spades.type).to eq(:A)
+      it "initializes the card with the correct suit and card type" do
+         expect(ace_spades.suit).to eq(:spades)
+         expect(ace_spades.type).to eq(:A)
 
+      end
    end
 
+   describe "Card#to_s" do 
+      it "returns the correct unicode suit and value for each card" do
+         expect(ace_spades.to_s).to eq("A♤")
+      end
 
+   end
+   
 
+   
 end
