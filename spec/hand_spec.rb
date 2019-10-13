f498d2e35e6089ad3e864dc78a43ddaa96c8c9b3
joshIsCoding <yes_you_do@hotmail.com>
2019-10-13 17:51:00 +0100

    Setup #initialize pending specs

--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -11,6 +11,13 @@ describe Hand do
          double(:value => :J, :suit => :hearts)
       ]
    end
+
+   describe "#initialize" do
+      new_hand = Hand.new(no_pair_hand)
+      it "Takes an array of cards and creates a hand"
+
+      it "Raises an error unless 5 cards are provided" 
+   end
    
 
 
